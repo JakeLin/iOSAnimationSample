f2467246caacec54c516f1f8fda7de2b4294cff1
Jake Lin <jake.lin@rea-group.com>
2015-04-29 23:44:04 +1000

    UIView Animation - Position
    Add Position animation to the project.

---
+++ iOSAnimationSample/PositionViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PositionViewController.swift
+//  iOSAnimationSample
+//
+//  Created by Jake Lin on 4/29/15.
+//  Copyright (c) 2015 JakeLin. All rights reserved.
+//
+
+import UIKit
+
+class PositionViewController: UIViewController {
+    @IBOutlet weak var blueSquare: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        // <null>
+        // println(self.view.actionForLayer(self.view.layer, forKey: "position"))
+
+        UIView.animateWithDuration(1, animations: {
+            self.blueSquare.center.x = self.view.bounds.width - self.blueSquare.center.x
+            // <_UIViewAdditiveAnimationAction: 0x7fe17b70df30>
+            // println(self.view.actionForLayer(self.view.layer, forKey: "position"))
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
