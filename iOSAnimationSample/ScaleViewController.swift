178970d4882f11f141ab8ee63442dab1bfc3117d
Jake Lin <jakelinau@gmail.com>
2015-04-30 23:16:51 +1000

    Add UIView Scale animation

---
+++ iOSAnimationSample/ScaleViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ScaleViewController.swift
+//  iOSAnimationSample
+//
+//  Created by Jake Lin on 4/30/15.
+//  Copyright (c) 2015 JakeLin. All rights reserved.
+//
+
+import UIKit
+
+class ScaleViewController: UIViewController {
+
+    @IBOutlet weak var blueSquare: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1, animations: {
+            self.blueSquare.transform = CGAffineTransformMakeScale(2.0, 2.0)
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
