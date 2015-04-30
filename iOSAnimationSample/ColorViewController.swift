6a134129f0283d9975e8701de85d0e761d5d1f5a
Jake Lin <jakelinau@gmail.com>
2015-04-30 23:31:16 +1000

    Add UIView Color animation

---
+++ iOSAnimationSample/ColorViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ColorViewController.swift
+//  iOSAnimationSample
+//
+//  Created by Jake Lin on 4/30/15.
+//  Copyright (c) 2015 JakeLin. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+
+    @IBOutlet weak var blueSquare: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1, animations: {
+            self.blueSquare.backgroundColor = UIColor.redColor()
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
