5651289c7fbe961f9328b2a0767c04c5d7e5010b
Jake Lin <jakelinau@gmail.com>
2015-05-02 22:36:41 +1000

    Add Rotation animation

---
+++ iOSAnimationSample/RotationViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RotationViewController.swift
+//  iOSAnimationSample
+//
+//  Created by Jake Lin on 5/1/15.
+//  Copyright (c) 2015 JakeLin. All rights reserved.
+//
+
+import UIKit
+
+class RotationViewController: UIViewController {
+
+    @IBOutlet weak var wheel: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1, animations: {
+            self.wheel.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
