88af92dd9b437929d070e906af74e6472c6c198b
Jake Lin <jakelinau@gmail.com>
2015-05-05 08:03:17 +1000

    UIView Repeat Animation

---
+++ iOSAnimationSample/RepeatViewController.swift
@@ -0,0 +1,53 @@
+//
+//  RepeatViewController.swift
+//  iOSAnimationSample
+//
+//  Created by Jake Lin on 5/5/15.
+//  Copyright (c) 2015 JakeLin. All rights reserved.
+//
+
+import UIKit
+
+class RepeatViewController: UIViewController {
+
+    @IBOutlet weak var blueSquare: UIView!
+    
+    @IBOutlet weak var redSquare: UIView!
+    
+    @IBOutlet weak var greenSquare: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1, animations: {
+            self.blueSquare.center.x = self.view.bounds.width - self.blueSquare.center.x
+        })
+        
+        UIView.animateWithDuration(1, delay: 0, options: .Repeat, animations: {
+            self.redSquare.center.x = self.view.bounds.width - self.redSquare.center.x
+        }, completion: nil)
+        
+        UIView.animateWithDuration(1, delay: 0, options: .Repeat | .Autoreverse, animations: {
+            self.greenSquare.center.x = self.view.bounds.width - self.greenSquare.center.x
+            }, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
