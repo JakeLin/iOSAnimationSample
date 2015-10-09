6ac77025dc65777bcb825807c43714dcae71cc13
Jake Lin <jakelinau@gmail.com>
2015-10-09 23:53:47 +1100

    Add AutoLayout Login VC

---
+++ iOSAnimationSample/AutoLayoutLoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AutoLayoutLoginViewController.swift
+//  iOSAnimationSample
+//
+//  Created by Jake Lin on 10/9/15.
+//  Copyright © 2015 JakeLin. All rights reserved.
+//
+
+import UIKit
+
+class AutoLayoutLoginViewController: UIViewController {
+
+}
